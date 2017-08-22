a4425547a5c0f494f802516ea832c1e8e325d2db
vinceq-usgs <vinceq@usgs.gov>
2017-08-22 11:15:07 -0700

    Move product list to lib/products.yml

--- dyfi/products.py
+++ dyfi/products.py
@@ -7,7 +7,7 @@ Products
 
 import json
 import os
-from collections import OrderedDict
+import yaml
 
 #from .plotmap import PlotMap
 from .plotgraph import PlotGraph
@@ -21,12 +21,11 @@ class Products:
     :param str name: type of product (e.g. 'geo_1km', 'zip')
     
         self.maps=None
-        self.rawentries=rawentries
+        self.entries=entries
         
         self.evid=event.eventid
         self.productDir=product dir
         self.products=[]        
-        self.data={}
 
     .. attribute:: evid
     
@@ -40,7 +39,7 @@ class Products:
     
     A reference to the Maps object which provides custom parameters for this product. (Not yet implemented)
 
-    .. attribute:: rawentries
+    .. attribute:: entries
     
     A reference to the Entries object for this product.
 
@@ -57,224 +56,136 @@ class Products:
     A dict of processed data. Each key is the name of the dataset and each value is a reference to the data.
     
     """
-    
-    # productTypes must be ordered because contents needs to be processed last.
-    productTypes=OrderedDict([           
-        ('map',{
-            'datatypes': ['geo_10km','geo_1km','zip'],
-            'formats':['geojson','png']
-            }),
-        ('graph',{
-            'datatypes': ['dist_vs_intensity','time_vs_responses'],
-            'formats':['geojson']
-            }),
-        ('contents',{
-            'formats':['xml']
-            })
-        ])
-        
-        
-    def __init__(self,event,maps,rawentries):
+   
+ 
+    def __init__(self,event,entries,config=None):
         self.event=event
-        # TODO: Use map parameters from DB table
         self.maps=None
-        self.rawentries=rawentries
+        self.entries=entries
+        self.config=config
  
         self.evid=event.eventid
-        self.productDir='data/' + self.evid
+        self.dir='data/' + self.evid
         self.products=[]        
-        self.productFiles=[]
-        self.data={}
+        self.data=[]
+
+        with open(config.products['file']) as f:
+          self.allProducts=yaml.load(f)
 
         
-    def create(self,ptype=None,dtype=None):
+    def createAll(self):
 
-        if not ptype:
-            for ptype in self.productTypes:
-                self.create(ptype,dtype)
-            return
+        count=0
+        for p in self.allProducts:
+            count+=self.create(**p)
+        return count
 
-        # At this point, ptype is specified (map, graph, or contents)
-    
-        productdir=self.productDir
-        os.makedirs(productdir,exist_ok=True)
-        # TODO: Check that this directory exists
 
-        dtypes=self.productTypes[ptype]
-        
-        if dtype:
-            if dtype in dtypes:
-                datatypes=[datatype]
-            else:
-                print('Products.create: No %s in %s, skipping.' % (dtype,ptype))
-        
-        if ptype=='map':
-            self.createMapProducts(ptype,dtypes)
-            
-        elif ptype=='graph':
-            self.createGraphProducts(ptype,dtypes)
-            
-        elif ptype=='contents':
-            contents=Contents.createXML(self.event,productdir,'contents.xml')
-            print('WARNING: must add contents to product list')
-            
-        # Add other product types here
-        
+    def create(self,name=None,type=None,dataset=None,format=None):
+        print('Making:',name,type,dataset,format)
+
+        if dataset:
+            data=self.getDataset(dataset)
+
+        # Set working directory 
+        os.makedirs(self.dir,exist_ok=True)
+
+        if type=='contents':
+            data=Contents(self.dir)
+
+        elif type=='graph':
+            data=Graph(name=name,event=self.events,data=data)
+
+        elif data:
+            pass
+
         else:
-            raise NameError('Unknown producttype '+ptype)
-        
-            
-    def createMapProducts(self,producttype,productlist):
-        
-        """
-        
-        :synopsis: Create map products.
-        :param str producttype: geo_1km, geo_10km, zip
-        :return: list of product filenames created
-        
-        """
+            raise nameError('Cannot create blank product')
 
-        dtypes=productlist['datatypes']
-        ftypes=productlist['formats']
-        
-        for dtype in dtypes:
-            
-            filename=self.productDir+'/dyfi_'+dtype+'.png'
-            print('Products: creating',filename)
+        # Now create multiple formats of data
+        count=0
+        formats=format.split(',') if (',' in format) else (format)
+        for format in formats:
+            product=Product(dir=self.dir,data=data,name=name).create(format)
+            if product:
+                self.products.append(product)
+                count+=1
 
-            dataset=self.rawentries.aggregate(dtype)
-
-            # TODO: Use map params
-
-#            plot=PlotMap(
-#                event=self.event,
-#                data=dataset)
-
-#            self.addProduct(plot.save(filename))
-#            self.addProduct(self.saveGeoJSON(dtype,dataset))
+        return count
    
-    
-    def createGraphProducts(self,producttype,productlist):
-        
-        """
-        
-        :synopsis: Create graph products.
-        :param str producttype: geo_1km, geo_10km, zip
-        :return: list of product filenames created
-        
-        """
 
-        dtypes=productlist['datatypes']
-        ftypes=productlist['formats']
-        
-        for dtype in dtypes:
-            
-            filename=self.productDir+'/dyfi_'+dtype+'.png'
-            print('Products: creating',filename)
+    def getDataset(self,name):
 
-            if dtype=='dist_vs_intensity':
-                print('Using set geo_10km aggregated data for',dtype,'plot.')
-                dataset=self.rawentries.aggregate('geo_10km')
-                
-            elif dtype=='time_vs_responses':
-                dataset=self.rawentries
-                
-            # TODO: Use map params
-            plot=PlotGraph(
-                event=self.event,
-                graphtype=dtype,
-                data=dataset)
-                                                   
-            self.addProduct(plot.save(filename))
-            self.addProduct(self.saveGeoJSON(dtype,dataset))
+        # Reuse precomputed data if possible
+        matches=[x for x in self.data if x.name==name]
 
-            
-    def createStaticMaps(self):
-        # OBSOLETE
-        """
-            :synopsis: Create a map using parameters from the mapList database table.
-            :return: list of created filenames
+        if len(matches)>0:
+            return matches[0]
 
-        """
-        mapparams=self.event.mapList()
-        filename=self.filename
-        out=[]
+        print('Creating dataset',name)
 
-        # Ignore zoom and zoomout maps for now.
-        # Only use the 'base' parameters.
-        
-        for maprow in mapparams:
-            mapid=maprow.mapid
-            if 'base' not in mapid:
-                continue
-                
-            if 'base' not in mapid and 'geo' not in mapid:
-                filename='dyfi_%s_%s%s'% (
-                    self.productType,mapid,self.productformat)
-
-            filename=self.productDir+'/'+filename
-            plot=Plot(filename,self.event,maprow,self.data)
-            filename=plot.create()
-            out.append(filename)
-
-            # TODO: Do PDF, IMAP products here
-            
-        return out
-      
-    
-    def saveGeoJSON(self,producttype,dataset):
-        """
-        
-        :synopsis: Create a GeoJSON product file of aggregated data.
-        :returns: the product filename created
-        
-        This will create a GeoJSON file of the data referenced 
-        by :py:obj:`data`.
-        
-        """
-        
-        filename=self.productDir+'/'+'dyfi_'+producttype+'.geojson'
-
-        try:
-          json.dump(rawentries,open(filename,'w'),indent=2)
-        except:
-          print('WARNING: Could not convert',producttype,'to JSON')
-        
-        return filename
-    
-    
-    def addProduct(self,product):
-        filename=None
-        
-        if isinstance(product,list):
-            for prod in product:
-                self.addProduct(prod)
-            return
-        
-        if isinstance(product,str):
-            filename=product
-        
-        elif hasattr(product,'filename'):
-            filename=product.filename
+        if 'geo' in name:
+           data=self.entries.aggregate(name)
 
         else:
-            raise NameError('Products.addProduct unknown product'+product)
+            raise NameError('Unknown data type '+name)
 
-        if filename in self.products:
-            print('WARNING: Product',product,'already exists.')
-                
-        else:
-            self.products.append(filename)
-            
-        return filename
-    
-    
+        self.data.append(data)
+        return data
+
+ 
     def __repr__(self):
         if len(self.productFiles)<1:
             return 'No products'
-        
-        for name in self.productFiles:
-            text='Product:['+','.join(self.productFiles)+']'
+       
+        text='' 
+        for product in self.products:
+            text+='Product:['+product.filename+']'
             return text
-        
-            
+
+    
+class Product:
+
+    def __init__(self,dir,data,name):
+        self.data=data
+        self.dir=dir
+        self.name=name
+
+
+    def create(self,format):
+        data=self.data
+
+        filename=self.dir+'/'+self.name+'.'+format
+        print('Writing:',filename)
+
+        if format=='json':
+            if hasattr(data,'toJSON'):            
+                product=data.toJSON()
+            else:
+                product=json.dumps(data)
+            with open(filename,'w') as f:
+                f.write(product)
+               
+
+        elif format=='geojson':
+            if hasattr(data,'toGeoJSON'):            
+              product=data.toGeoJSON()
+            else:
+              product=json.dumps(data)
+            with open(filename,'w') as f:
+                f.write(product)
+
+        elif format=='png':
+            if hasattr(data,'toImage'):            
+              product=data.toImage()
+            else:
+              product={}
+
+        else:
+            raise NameError('Unknown format '+format)
+
+
+        self.filename=filename
+        self.product=product
+        return self
+
