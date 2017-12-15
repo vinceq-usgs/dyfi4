014663b1d8bb51841ad7931cdffd9e9d2a94d6c0
vinceq-usgs <vinceq@usgs.gov>
2017-12-15 12:52:16 -0800

    Now accept input from file, for debugging

--- app/readEvent.py
+++ app/readEvent.py
@@ -20,6 +20,10 @@ parser.add_argument(
     help='Event ID'
 )
 parser.add_argument(
+    '--file',action='store',
+    help='Read from file instead of feed'
+)
+parser.add_argument(
     '--raw',action='store_true',
     help='Print raw feed and exit'
 )
@@ -36,12 +40,17 @@ parser.add_argument(
 def main(args):
 
     sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
-    from dyfi import Config,ComcatEvent,Event
+    from dyfi import Config,ComcatEvent,Event,Db
 
     evid=args.evid
 
-    event=ComcatEvent(evid)
-    contents=event.contents
+    if args.file:
+        with open(args.file,'r') as f:
+            contents=json.load(f)
+
+    else:
+        event=ComcatEvent(evid)
+        contents=event.contents
 
     if args.raw:
         print(json.dumps(contents,indent=2))
@@ -52,9 +61,16 @@ def main(args):
         print('Event ID changed from %s to %s' % (evid,event.eventid))
         evid=event.eventid
 
+    print('Event data:')
+    print(event)
+
     if args.save:
         print('Saving to event table.')
-        event.save(config=Config(args.config))
+        db=Db(Config(args.config))
+        if not db.save(event):
+            print('Warning: problem with saving this event.')
+
+    print('Done with',evid)
 
 
 if __name__=='__main__':
