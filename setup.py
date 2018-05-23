ef1ea24983f698a5de4d41cf7c0de9a91a375eda
vinceq-usgs <vinceq@usgs.gov>
2018-05-22 20:03:10 -0700

    Bring up repo root directory files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 # Copied from usgs/pager repo, not yet tested
 
 setup(name='dyfi',
-      version='0.0.2',
+      version='0.0.4',
       description='Did You Feel It? (DYFI)',
       author='Vince Quitoriano',
       author_email='vinceq@usgs.gov',
