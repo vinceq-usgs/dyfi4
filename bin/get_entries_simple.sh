3aeef6d35abf2bab530d0abd2683a51a2ac38599
vinceq <vinceq@usgs.gov>
2018-06-11 22:57:09 -0600

    Codacy 6

--- bin/get_entries_simple.sh
+++ bin/get_entries_simple.sh
@@ -6,11 +6,11 @@
 #
 #############################################
 
-MYHOST='examplehost'
+MYHOST='dyfit'
 declare -a SERVERS
-SERVERS=(host1.com host2.com)
-INPUTDIR='/var/inputdirectory/incoming.'$MYHOST
-TIMEOUT=10
+SERVERS=(vinceq@igskahcgvmp1ct1.cr.usgs.gov vinceq@igskmncgvmp2ct1.cr.usgs.gov)
+INPUTDIR='/var/www/data/earthquake-dyfi-response/incoming.vm'$MYHOST
+TIMEOUT=15
 DESTINATION=./data/incoming
 
 #############################################
@@ -23,8 +23,8 @@ for SERVER in "${SERVERS[@]}"
 do
     getcommand="/usr/bin/rsync --verbose --exclude 'tmp.*' --timeout=$TIMEOUT -ulpotgrz $SERVER:$INPUTDIR/. $DESTINATION"
     echo $getcommand
-    echo `$getcommand`
+    echo $($getcommand)
 done
 
 echo 'Entries in '$DESTINATION':'
-echo `ls -1 $DESTINATION | wc | awk '{print $1}'`
+echo $(ls -1 $DESTINATION | wc | awk '{print $1}')
