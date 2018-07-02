63e7ae29d32a8bb468484079c11e72f2928a23f0
vinceq-usgs <vinceq@usgs.gov>
2018-07-02 14:52:01 -0700

    Add other directories to Travis setup for codecov

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 # Copied from usgs/pager repo, not yet tested
 
 setup(name='dyfi',
-      version='0.0.4',
+      version='0.0.5',
       description='Did You Feel It? (DYFI)',
       author='Vince Quitoriano',
       author_email='vinceq@usgs.gov',
       url='',
-      packages=['dyfi'],
-      scripts=['app/rundyfi.py','app/makescreenshot.py','app/makemovie.py']
+      packages=['dyfi']['util/modules'],
+      scripts=['app/rundyfi.py','app/makescreenshot.py','app/makemovie.py','util/queueTriggers.py','util/loadEntries.py','util/updateEvent.py']
 )
