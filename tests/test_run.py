2336011a4abdee0553e6fd0b39c2e4df71995ec1
vinceq-usgs <vinceq@usgs.gov>
2018-07-02 15:36:43 -0700

    Codecov 1

--- tests/test_run.py
+++ tests/test_run.py
@@ -20,22 +20,6 @@ def test_lock():
 
 
 def test_run():
-    from app import rundyfi
-
-    if testid:
-        shutil.rmtree('data/'+testid,ignore_errors=True)
-
-    with pytest.raises(RuntimeError) as exception:
-        rundyfi.main(Namespace(evid='blank',configfile=configfile))
-    assert 'Cannot create Event' in str(exception.value)
-
-    container=rundyfi.main(Namespace(evid=testid,configfile=configfile))
-    products=container.products
-    assert len(products.products)==6
-    assert 'Products' in str(products)
-
-    from dyfi import Contents
-    contents=Contents(container)
-    data=contents.toXML(save=False)
-    assert 'dyfi_geo_10km.png' in data
+    import subprocess
+    subprocess.run(['app/rundyfi.py',testid])
 
