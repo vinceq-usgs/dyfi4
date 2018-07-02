926000c5efcc939f1c9e622a7f5ce5937f38c387
vinceq <vinceq@usgs.gov>
2018-07-02 11:12:40 -0600

    Typo

--- dyfi/pending.py
+++ dyfi/pending.py
@@ -119,7 +119,7 @@ class Pending:
     def checkRunRecent(self,row):
 
         if not 'process_timestamp' in row or not row['process_timestamp']:
-            print('pending.checkRunRecent: WARNING: No process timestamp for event',ro['eventid'])
+            print('pending.checkRunRecent: WARNING: No process timestamp for event',row['eventid'])
             return None
 
         age=self.db.stringToAge(row['process_timestamp'])
